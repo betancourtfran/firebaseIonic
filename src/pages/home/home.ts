2a2993d6e06c672a0609bd8980e9bb0aea417648
Francisco Betancourt <betancourtfran@gmail.com>
2017-04-24 14:15:41 -0400

    added update, remove and showOptions

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { AngularFire, FirebaseListObservable } from "angularfire2";
+import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 
 @Component({
@@ -9,8 +9,101 @@ import { AngularFire, FirebaseListObservable } from "angularfire2";
 })
 export class HomePage {
   songs: FirebaseListObservable<any>;
-  constructor(public navCtrl: NavController, public af:AngularFire) {
+  constructor(public navCtrl: NavController, public af:AngularFire, public alertCtrl: AlertController, public actionSheetCtrl: ActionSheetController) {
     this.songs = af.database.list('/songs');
-  }
+  };
 
+  addSong(){
+    let prompt = this.alertCtrl.create({
+      title: "Song Name",
+      message: "Enter a name for this new song you're so keen on adding",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        }
+      ],
+      buttons: [
+        {
+          text:'Cancel',
+          handler: data => {
+            console.log('Cancel Clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data =>{
+            this.songs.push({
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  };
+
+  showOptions(songId, songTitle) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'qué quieres hacer?',
+      buttons: [
+        {
+          text:'Borrar Cancion',
+          role: 'destructive',
+          handler: () => {
+            this.removeSong(songId);
+          }
+        },
+        {
+          text: 'Actualizar Titulo',
+          handler: () => {
+            this.updateSong(songId, songTitle);
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel Clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  };
+  removeSong(songId: string){
+    this.songs.remove(songId).then(() => {
+      console.log('cancion eliminada');
+    });
+  };
+  updateSong(songId, songTitle){
+    let prompt = this.alertCtrl.create({
+      title: 'Nombre de la cancion',
+      message: "Actualiza el nombre para esta cancion",
+      inputs: [
+        {
+          name: 'titulo',
+          placeholder: 'Titulo',
+          value: songTitle
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: data => {
+            console.log('Cancel CLicked');
+          }
+        },
+        {
+          text: 'Guardar',
+          handler: data => {
+            this.songs.update(songId, {
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
